35e1bdaf90dc666cc2a67abb656398e2a6596082
madvolume <gleb.generalyuk@ed-era.com>
2015-06-25 11:42:50 +0300

    upd

---
+++ custom/js/popup.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+   $( "div.popup" ).replaceWith( "<div class='" + "description" + "'><div class='" + "text" +"'>Ще більше відео матеріалів в <a class='"+"knopka1"+"' href='" + "https://study.ed-era.com/courses/EdEra/P101/p101/about" + "' target='" + "_blank" + "'>онлайн-курсі</a> від розробників цієї книги</div><div class='" + "btn-knopka" +"'><a class='"+"knopka"+"' href='" + "https://study.ed-era.com/courses/EdEra/P101/p101/about" + "'   target='" + "_blank" + "'>Перейти на курс</a></div>" );
+   });
