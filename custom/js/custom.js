96c7d39299c94e8fc38de04d66cfab96b9d644e3
chudaol <chudaol@gmail.com>
2015-08-20 13:41:59 +0200

    Update custom/js/custom.js

--- custom/js/custom.js
+++ custom/js/custom.js
@@ -1,3 +1,8 @@
 $(document).ready(function () {
+  var href;
+  
   $("a.toggle-search").remove();
+  href = $(".custom-link").prop("href");
+  href = "http://" + _.last(href.split("-")) + ".ed-era.com";
+  $(".custom-link").prop("href", href);
 });
