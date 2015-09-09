3f46b1299accebdada5ab6f70a67eb38e5764f56
chudaol <chudaol@gmail.com>
2015-09-09 14:41:23 +0200

    Update custom/js/custom.js

--- custom/js/custom.js
+++ custom/js/custom.js
@@ -1,9 +1,10 @@
 $(document).ready(function () {
-  var href;
+  var href, $lastLink;
   
   $("a.toggle-search").remove();
-  href = $(".custom-link:last").prop("href");
-  //href = "https://ed-era.com/books/" + _.last(href.split("-"));
-  href = "http://" + _.last(href.split("-")) + ".ed-era.com";
-  $(".custom-link:last").prop("href", href);
+  
+  $lastLink = $(".custom-link:last");
+  href = $lastLink.prop("href");
+  href = "https://ed-era.com/books/" + _.last(href.split("-"));
+  $lastLink.prop("href", href);
 });
